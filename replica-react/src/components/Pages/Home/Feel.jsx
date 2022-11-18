0301e61e77d2b3d6dd3dda6b25c66d7e6bc11e12
LidiaLG <lidialuquegallego91@gmail.com>
2022-11-18 20:15:42 +0100

    medio responsive

--- replica-react/src/components/Pages/Home/Feel.jsx
+++ replica-react/src/components/Pages/Home/Feel.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import '../Home/Feel.scss'
+import Girl from '../../../Assets/landing-page-girl.png'
 
 
 const Feel = () => {
   return (
     <div className="text">
-        <h1>Feel The Music</h1>
-        <p>Stream over 20 thousand songs with one click</p>
-        <button className="joinBtn">Join Now</button>
+        <img src={Girl} class="pictureGirl" alt="Girl"></img>
+        <div className="containerR">
+          <h1>Feel The Music</h1>
+          <p>Stream over 20 thousand songs with one click</p>
+          <button className="joinBtn">Join Now</button>
+        </div>
     </div>
   )
 }
